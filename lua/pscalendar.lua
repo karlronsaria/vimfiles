441e410556023250eaef4d783eb08cd84ed45ae3
karlronsaria <andrew.mikedaniels@gmail.com>
2022-10-17 22:33:02 -0700

    fix(pscalendar): issue: failure text in editor
    actual: PowerShell script displays error message due to unrelated
    failure to load profile. Error message appears in editor.
    expected: Failure text appears in standard output and intended text
    appears in editor.

--- lua/pscalendar.lua
+++ lua/pscalendar.lua
@@ -5,12 +5,21 @@ vim.api.nvim_create_user_command(
   'Calendar',
   function()
     local cmd = script_path() .. 'nvim/pwsh/Get-DateStringFromCalendar.ps1'
-    local dateStr = ToSingleLine(RunPowerShell(cmd), '')
 
-    if (cmd == '') then
+    if ('' == cmd) then
       return
     end
 
+    local dateStr = ToSingleLine(RunPowerShell(cmd), '')
+
+    print(dateStr)
+    local start, len = string.find(dateStr, '%d%d%d%d_%d%d_%d%d')
+
+    if (nil == start) then
+        return
+    end
+
+    dateStr = string.sub(dateStr, start, len)
     vim.api.nvim_put({ dateStr }, 'c', true, true)
   end,
   { nargs = 0 }
