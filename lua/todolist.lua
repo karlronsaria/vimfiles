dba28cec97488c3ee6a1d2c3626f12227ab61e3f
karlronsaria <andrew.mikedaniels@gmail.com>
2023-09-28 22:29:28 -0700

    feat(todolist): Todo

--- lua/todolist.lua
+++ lua/todolist.lua
@@ -1,25 +1,38 @@
 require('define')
 
+-- [[/\v^(\s*(\*|\-|\d+\.|[^\w\s]+))? \[\zs[^\]\[]{1,3}\ze]] .. '\\]' .. special('<CR>'),
+
+-- -- OLD (karlr 2023_09_28_210418)
+-- -- -----------------------------
+-- -- includes non-checkbox items and item tokens that cannot be justified,
+-- -- such as strings with brackets in them and anything having a length
+-- -- higher than 3
+-- -- - ex: ``- [x] see [``todo A``](#todo)``
+-- --            ~~~~~~~~~~~~~~~~~~
+-- [[/\v^(\s*(\*|\-|\d+\.|[^\w\s]+))? \[\zs.+\ze]] .. '\\]' .. special('<CR>'), 'm', true
+
+-- -- OLD (karlr 2023_09_28_210253)
+-- -- -----------------------------
+-- -- ignores program annotations (comments)
+-- [[/\v^(\s*(\*|\-|\d+\.))? \[\zs.+\ze]] .. '\\]' .. special('<CR>'), 'm', true
+
+local pattern = [[/\v^(\s*(\*|\-|\d+\.|[^\w\s]+))? \[\zs<token>\ze]] .. '\\]'
+
 vim.api.nvim_create_user_command(
   'Item',
   function()
     vim.api.nvim_feedkeys(
-      [[/\v^(\s*(\*|\-|\d+\.|[^\w\s]+))? \[\zs[^\]\[]{1,3}\ze]] .. '\\]' .. special('<CR>'),
-      'm', true
+      pattern:gsub("<token>", [[[^\]\[]{1,3}]]) .. special('<CR>'), 'm', true
+    )
+  end,
+  { nargs = 0 }
+)
 
-      -- -- OLD (karlr 2023_09_28_210418)
-      -- -- -----------------------------
-      -- -- includes non-checkbox items and item tokens that cannot be justified,
-      -- -- such as strings with brackets in them and anything having a length
-      -- -- higher than 3
-      -- -- - ex: ``- [x] see [``todo A``](#todo)``
-      -- --            ~~~~~~~~~~~~~~~~~~
-      -- [[/\v^(\s*(\*|\-|\d+\.|[^\w\s]+))? \[\zs.+\ze]] .. '\\]' .. special('<CR>'), 'm', true
-
-      -- -- OLD (karlr 2023_09_28_210253)
-      -- -- -----------------------------
-      -- -- ignores program annotations (comments)
-      -- [[/\v^(\s*(\*|\-|\d+\.))? \[\zs.+\ze]] .. '\\]' .. special('<CR>'), 'm', true
+vim.api.nvim_create_user_command(
+  'Todo',
+  function()
+    vim.api.nvim_feedkeys(
+      pattern:gsub("<token>", [[ ]]) .. special('<CR>'), 'm', true
     )
   end,
   { nargs = 0 }
