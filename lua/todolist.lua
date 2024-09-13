5733ad668448655465c8745df8cbf3e62d107fec
karlr <andrew.mikedaniels@gmail.com>
2024-09-13 02:32:30 -0700

    chore: code cleanup

--- lua/todolist.lua
+++ lua/todolist.lua
@@ -1,9 +1,5 @@
 require('define')
 
-function is_in_vscode()
-    return vim.fn.getenv("VSCODE_PID") ~= vim.NIL
-end
-
 -- [[/\v^(\s*(\*|\-|\d+\.|[^\w\s]+))? \[\zs[^\]\[]{1,3}\ze]] .. '\\]' .. special('<CR>'),
 
 -- -- OLD (karlr 2023_09_28_210418)
