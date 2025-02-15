423426f4faed59c1423b21e0fc8ea9a0af3110d9
karlr <andrew.mikedaniels@gmail.com>
2025-02-15 06:22:44 -0800

    feat: python lsp

--- lua/nvim-lsp.lua
+++ lua/nvim-lsp.lua
@@ -1,9 +1,22 @@
 local lspconfig = require('lspconfig')
 
 -- Setup language servers
-lspconfig.clangd.setup{}      -- C++
-lspconfig.rust_analyzer.setup{} -- Rust
-lspconfig.lua_ls.setup{}       -- Lua
+lspconfig.clangd.setup{}  -- C++
+lspconfig.rust_analyzer.setup{}  -- Rust
+lspconfig.lua_ls.setup{}  -- Lua
+lspconfig.pyright.setup{  -- Python
+    settings = {
+        pyright = {
+            plugins = {
+                jedi_completion = { enabled = true },
+                rope_completion = { enabled = true },
+                pylint = { enabled = true },
+                mypy = { enabled = true, live_mode = true },
+                pycodestyle = { enabled = true }
+            }
+        }
+    }
+}
 
 -- Keybindings
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
