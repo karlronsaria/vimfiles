b1bc0604d7689c98b93a9a9c06e81a2b7d4617f6
karlr <andrew.mikedaniels@gmail.com>
2026-02-10 05:23:44 -0800

    fix(lsp): update deprecated config

--- lua/nvim-lsp.lua
+++ lua/nvim-lsp.lua
@@ -1,14 +1,14 @@
-local lspconfig = require('lspconfig')
+-- local lspconfig = require('lspconfig')
 local home_directory = os.getenv('USERPROFILE')
 
 -- Setup language servers
-lspconfig.clangd.setup{}  -- C++
-lspconfig.rust_analyzer.setup{}  -- Rust
-lspconfig.powershell_es.setup{  -- PowerShell
+vim.lsp.config('clangd', {})  -- C++
+vim.lsp.config('rust_analyzer', {})  -- Rust
+vim.lsp.config('powershell_es', {  -- PowerShell
     bundle_path = home_directory .. '/OneDrive/Documents/PowerShell/Modules/PowerShellEditorServices'
-}
-lspconfig.lua_ls.setup{}  -- Lua
-lspconfig.pyright.setup{  -- Python
+})
+vim.lsp.config('lua_ls', {})  -- Lua
+vim.lsp.config('pyright', {  -- Python
     settings = {
         pyright = {
             plugins = {
@@ -20,8 +20,22 @@ lspconfig.pyright.setup{  -- Python
             }
         }
     }
-}
-lspconfig.sqlls.setup{}  -- SQL
+})
+vim.lsp.config('sqlls', {})  -- SQL
+vim.lsp.config('omnisharp', {})  -- C#
+vim.lsp.config('csharpier', {})  -- C#
+vim.lsp.config('julia-lsp', {})  -- Julia
+
+-- Enable language servers
+vim.lsp.enable('clangd')
+vim.lsp.enable('rust_analyzer')
+vim.lsp.enable('powershell_es')
+vim.lsp.enable('lua_ls')
+vim.lsp.enable('pyright')
+vim.lsp.enable('sqlls')
+vim.lsp.enable('omnisharp')
+vim.lsp.enable('csharpier')
+vim.lsp.enable('julia-lsp')
 
 -- Keybindings
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
